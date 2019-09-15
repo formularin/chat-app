6580ff36d40cce308a1b59bdaa644692c954cb1d
lol-cubes <arinmkhare@gmail.com>
2019-09-15 10:32:17 -0400

    allow communication between 2 clients

--- chat-app/client.py
+++ chat-app/client.py
@@ -1,12 +1,30 @@
 import socket
+import threading
 
 server = input()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server, 1235))
+s.connect((server, 1237))
 
-while True:
-    msg = input()
-    s.send(bytes(msg, "utf-8"))
-    data = s.recv(1024)
-    print("client:", data.decode("utf-8"))
+def send_messages():
+    """
+    Sends input to server which
+    sends to other clients
+    """
+    while True:
+        msg = input()
+        s.send(bytes(msg, "utf-8"))
+
+def receive_messages():
+    """
+    Constantly receives data from
+    server and prints to console
+    """
+    while True:
+        data = s.recv(1024)
+        print(data.decode("utf-8"))
+
+sm = threading.Thread(target=send_messages)
+rm = threading.Thread(target=receive_messages)
+sm.start()
+rm.start()
--- chat-app/server.py
+++ chat-app/server.py
@@ -1,6 +1,7 @@
 import socket
 import signal
 import sys
+import threading
 
 # ^c catching
 def signal_handler(sig, frame):
@@ -9,17 +10,40 @@ signal.signal(signal.SIGINT, signal_handler)
 
 # create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("localhost", 1235))
+s.bind(("localhost", 1237))
 s.listen(5)
+
+clientsockets = []
+addresses = []
+
+def handle_client_message(clientsocket, address):
+    """
+    Send recieved message back to 
+    client and also print to console
+    """
+    while True:
+        msg = clientsocket.recv(1024)
+        if msg != b'':
+            msg = f"{address}: " + msg.decode("utf-8")
+
+            with open('/Users/Mukeshkhare/Desktop/thing.txt', 'w+') as f:
+                f.write(msg)
+
+            for c in clientsockets:
+                c.send(bytes(msg, "utf-8"))
+
+            print(msg)
 
 while True:
     
     # make connection
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    
-    # send messages
-    while True:
-        msg = clientsocket.recv(1024)
-        clientsocket.send(msg)
-        print(f"{address}:", msg.decode("utf-8"))
+    clientsockets.append(clientsocket)
+    addresses.append(address)
+
+
+    # create thread to constantly be looking for new connections, but still respond to messages
+    x = threading.Thread(target=handle_client_message, args=(clientsocket, address))
+    x.start()
+
