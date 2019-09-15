1609f6c99584c77a0d62d7ca877be51898c5452c
lol-cubes <arinmkhare@gmail.com>
2019-09-14 21:46:09 -0400

    have client send data

--- chat-app/client.py
+++ chat-app/client.py
@@ -6,7 +6,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server, 1235))
 
 while True:
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
-
-
+    msg = input()
+    s.send(bytes(msg, "utf-8"))
+    data = s.recv(1024)
+    print("client:", data.decode("utf-8"))
--- chat-app/server.py
+++ chat-app/server.py
@@ -2,17 +2,24 @@ import socket
 import signal
 import sys
 
+# ^c catching
 def signal_handler(sig, frame):
     sys.exit()
 signal.signal(signal.SIGINT, signal_handler)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(("localhost", 1235))
-    s.listen()
+# create socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("localhost", 1235))
+s.listen(5)
 
+while True:
+    
+    # make connection
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-
+    
+    # send messages
     while True:
-        msg = input()
-        clientsocket.send(bytes(msg, "utf-8"))
+        msg = clientsocket.recv(1024)
+        clientsocket.send(msg)
+        print(f"{address}:", msg.decode("utf-8"))
