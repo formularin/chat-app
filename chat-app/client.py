a962b6e89aaf0a052db0c840a5e95ab14714c588
lol-cubes <arinmkhare@gmail.com>
2019-09-15 14:45:55 -0400

    merge to allow revert

--- chat-app/client.py
+++ chat-app/client.py
@@ -1,37 +1,6 @@
 import curses
 import socket
 
-
-class Canvas:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.grid = [[" " for _ in range(self.width)] for _ in range(self.height())]
-    @property
-
-    def display(self):
-        return "\n".join(["".join(row) for row in self.grid])
-
-    def replace(self, x, y, char):
-        self.grid[y][x] = char
-
-
-class Message:  
-    def __init__(self, x, y, text, canvas):
-        self.x = x
-        self.y = y
-        self.text = text
-        self.canvas = canvas
-    
-    def render(self):
-        for char in self.text:
-            self.canvas.replace(
-                self.x,
-                self.y + self.text.index(char),
-                char
-            )
-
-
 server = input()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
