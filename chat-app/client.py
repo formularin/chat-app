8dcf4494843a9f8c9d1f9d97ec6546a772d7ad95
lol-cubes <arinmkhare@gmail.com>
2019-10-09 09:15:47 -0400

    create homescreen

--- chat-app/client.py
+++ chat-app/client.py
@@ -4,16 +4,32 @@ import signal
 import socket
 import os
 import threading
+import time
 
 from cryptography.fernet import Fernet
 
+import graphics
+import art
 
 def signal_handler(sig, frame):
     os._exit(1)
 
+def home_screen(stdscr):
+
+    while True:
+
+        key = stdscr.getch()
+        if key != -1:
+            return
+
+        stdscr.clear()
+        stdscr.addstr(art.HOME_SCREEN)
+        stdscr.refresh()
+
 
 def main(stdscr):
-    pass
+    curses.curs_set(0)
+    curses.wrapper(home_screen)
 
 
 if __name__ == "__main__":
